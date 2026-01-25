14cda19cdabf7ec51a6ed5df8601e7e50d13d4cd
Luca Tassinari <luca.tassinari.2000@gmail.com>
2026-01-25 18:00:24 +0100

    feat: add mqtt network

--- examples/src/it/unibo/pslab/PingPong.scala
+++ examples/src/it/unibo/pslab/PingPong.scala
@@ -5,37 +5,50 @@ import cats.effect.std.Console
 import it.unibo.pslab.peers.Peers.*
 import it.unibo.pslab.peers.Peers.Quantifier.*
 import cats.Monad
-// import it.unibo.pslab.multiparty.MultiPartyLanguage.*
 import it.unibo.pslab.multiparty.MultiParty
 import it.unibo.pslab.multiparty.MultiParty.*
 import it.unibo.pslab.multiparty.Environment
 // import cats.Id
-import it.unibo.pslab.network.Network
-import it.unibo.pslab.network.InMemoryNetwork
 import cats.effect.IOApp
 import cats.effect.IO
-// import cats.Defer
+import it.unibo.pslab.network.mqtt.MqttNetwork
+import cats.effect.kernel.Temporal
+import scala.concurrent.duration.DurationInt
+import it.unibo.pslab.network.Codable
+import it.unibo.pslab.PingPong.*
 
-object Multiparty:
+object PingPong:
   type Pinger <: { type Tie <: Single[Ponger] }
   type Ponger <: { type Tie <: Single[Pinger] }
 
-  def pingPongProgram[F[_]: {Monad, Console}](using lang: MultiParty[F]): F[Unit] = for
+  def pingPongProgram[F[_]: {Monad, Console, Temporal}](using
+      lang: MultiParty[F],
+      codable: Codable[F, Int, lang.Format],
+  ): F[Unit] = for
     initial <- on[Pinger](0.pure)
     _ <- pingPong(initial)
   yield ()
 
-  def pingPong[F[_]: {Monad, Console}](using lang: MultiParty[F])(initial: Int on Pinger): F[Unit] = for
-    onPonger <- comm[Pinger, Ponger](initial)
-    _ <- Console[F].println(s"Pinger sent value to Ponger")
-    newCounter <- on[Ponger]:
-      take(onPonger).map(_ + 1)
-    newCounterOnPinger <- comm[Ponger, Pinger](newCounter)
-    result <- on[Pinger]:
-      take(newCounterOnPinger).map(_ + 1)
-    _ <- Console[F].println(s"Ponger sent value to Pinger")
-    _ <- pingPong(result)
-  yield ()
+  def pingPong[F[_]: {Monad, Console, Temporal}](using
+      lang: MultiParty[F],
+      codable: Codable[F, Int, lang.Format],
+  )(initial: Int on Pinger): F[Unit] =
+    for
+      onPonger <- comm[Pinger, Ponger](initial)
+      newCounter <- on[Ponger]:
+        for
+          v <- take(onPonger)
+          _ <- Console[F].println(s"Ponger received value: $v")
+        yield v + 1
+      newCounterOnPinger <- comm[Ponger, Pinger](newCounter)
+      result <- on[Pinger]:
+        for
+          v <- take(newCounterOnPinger)
+          _ <- Console[F].println(s"Pinger received value: $v")
+        yield v + 1
+      _ <- Temporal[F].sleep(1.second)
+      _ <- pingPong(result)
+    yield ()
 
 // @main
 // def main(): Unit =
@@ -45,10 +58,24 @@ object Multiparty:
 //   val program = Multiparty.pingPongProgram[Id](using summon[Monad[Id]], summon[Console[Id]], lang)
 //   program
 
-object PingPongIOApp extends IOApp.Simple:
-  override def run: IO[Unit] =
-    val env = Environment.make[IO]
-    val network: Network[IO, Multiparty.Pinger] = InMemoryNetwork.make[IO, Multiparty.Pinger]
-    val lang = MultiParty.make[IO, Multiparty.Pinger](env, network)
-    val program = Multiparty.pingPongProgram[IO](using summon[Monad[IO]], summon[Console[IO]], lang)
-    program
+given Codable[IO, Int, Array[Byte]] = new Codable[IO, Int, Array[Byte]]:
+  override def encode(value: Int): IO[Array[Byte]] = IO.pure(value.toString.getBytes)
+  override def decode(data: Array[Byte]): IO[Int] = IO.pure(new String(data).toInt)
+
+object Pinger extends IOApp.Simple:
+  override def run: IO[Unit] = MqttNetwork
+    .localBroker[IO, PingPong.Pinger]("pinger-peer")
+    .use: network =>
+      val env = Environment.make[IO]
+      val lang = MultiParty.make(env, network)
+      val program = pingPongProgram[IO](using summon[Monad[IO]], summon[Console[IO]], summon[Temporal[IO]], lang)
+      program
+
+object Ponger extends IOApp.Simple:
+  override def run: IO[Unit] = MqttNetwork
+    .localBroker[IO, PingPong.Ponger]("ponger-peer")
+    .use: network =>
+      val env = Environment.make[IO]
+      val lang = MultiParty.make(env, network)
+      val program = pingPongProgram[IO](using summon[Monad[IO]], summon[Console[IO]], summon[Temporal[IO]], lang)
+      program
---
+++ examples/src/it/unibo/pslab/network/mqtt/MqttNetwork.scala
@@ -0,0 +1,130 @@
+package it.unibo.pslab.network.mqtt
+
+import scala.concurrent.duration.{ DurationLong, FiniteDuration }
+
+import it.unibo.pslab.multiparty.Environment.Reference
+import it.unibo.pslab.network.{ DecodableFrom, EncodableTo, Network }
+import it.unibo.pslab.network.Decodable.decode
+import it.unibo.pslab.network.Encodable.encode
+import it.unibo.pslab.peers.Peers.{ Peer, PeerTag }
+
+import cats.effect.kernel.{ Concurrent, Ref, Resource, Temporal }
+import cats.effect.std.{ Console, Queue }
+import cats.effect.syntax.all.*
+import cats.syntax.all.*
+import com.comcast.ip4s.{ Host, Port }
+import fs2.io.net.Network as Fs2Network
+import net.sigusr.mqtt.api.{ Message, Session, SessionConfig, TransportConfig }
+import net.sigusr.mqtt.api.QualityOfService.AtLeastOnce
+import upickle.default as upickle
+
+import upickle.ReadWriter
+
+object MqttNetwork:
+
+  case class Address(tag: String, clientId: String) derives ReadWriter
+
+  object Topics:
+    val presenceTopic = "pslab/presence"
+    val peersBaseTopic = "pslab/peers"
+    val valuesTopic = (tag: String, clientId: String) => s"$peersBaseTopic/$tag/$clientId"
+
+  import Topics.*
+
+  type MqttNetwork[F[_], LP <: Peer] = Network[F, LP] { type Format = Array[Byte] }
+
+  def localBroker[F[_]: {Concurrent, Temporal, Fs2Network, Console}, LP <: Peer: PeerTag as localPeerTag](
+      clientId: String,
+  ): Resource[F, MqttNetwork[F, LP]] = make(
+    clientId,
+    TransportConfig(Host.fromString("localhost").get, Port.fromInt(1883).get),
+    SessionConfig(clientId),
+  )
+
+  def make[F[_]: {Concurrent, Temporal, Fs2Network, Console}, LP <: Peer: PeerTag as localPeerTag](
+      clientId: String,
+      transportConfig: TransportConfig[F],
+      sessionConfig: SessionConfig,
+  ): Resource[F, MqttNetwork[F, LP]] =
+    for
+      session <- Session(transportConfig, sessionConfig)
+      incomingMsgs <- Resource.eval(Ref.of[F, Map[Address, Queue[F, Array[Byte]]]](Map.empty)) // TODO: add Reference
+      activePeers <- Resource.eval(Ref[F].of(Map.empty[Address, Long]))
+      msgsTopic = valuesTopic(localPeerTag.toString(), clientId)
+      _ <- Resource.eval(session.subscribe(List(msgsTopic, presenceTopic).map((_, AtLeastOnce)).toVector))
+      network = MqttNetworkImpl(clientId, session, incomingMsgs, activePeers)
+      _ <- network.publishPresence().background
+      _ <- network.handleIncomingMessage.background
+    yield network
+
+  private class MqttNetworkImpl[F[_]: {Concurrent, Temporal, Fs2Network, Console}, LP <: Peer: PeerTag as localPeerTag](
+      clientId: String,
+      session: Session[F],
+      queues: Ref[F, Map[Address, Queue[F, Array[Byte]]]],
+      activePeers: Ref[F, Map[Address, Long]],
+  ) extends Network[F, LP]:
+    override type Format = Array[Byte]
+
+    override type Address[P <: Peer] = MqttNetwork.Address
+
+    override val localAddress: Address[LP] = Address(localPeerTag.toString(), clientId)
+
+    val msgsTopic = valuesTopic(localPeerTag.toString(), clientId)
+
+    private[MqttNetwork] def handleIncomingMessage: F[Unit] = session.messages
+      .evalMap:
+        case Message(`presenceTopic`, data) =>
+          for
+            now <- Temporal[F].realTime.map(_.toMillis)
+            peerAddress = upickle.read[Address[?]](data.toArray)
+            _ <- activePeers.update(_.updated(peerAddress, now))
+          yield ()
+        case Message(`msgsTopic`, data) =>
+          for
+            (address, payload) = upickle.read[(Address[?], Array[Byte])](data.toArray)
+            _ <- getOrCreate(address).flatMap(_.offer(payload))
+          yield ()
+        case _ => Concurrent[F].unit
+      .compile
+      .drain
+
+    private[MqttNetwork] def publishPresence(interval: FiniteDuration = 500.millis): F[Unit] = (
+      session.publish(
+        presenceTopic,
+        upickle.write(localAddress).getBytes().toVector,
+        AtLeastOnce,
+      ) >> Temporal[F].sleep(interval)
+    ).foreverM
+
+    override def alivePeersOf[RP <: Peer: PeerTag as remotePeerTag]: F[Iterable[Address[RP]]] =
+      for
+        now <- Temporal[F].realTime.map(_.toMillis)
+        peersMap <- activePeers.get
+        res = peersMap.collect:
+          case (addr, lastSeen) if now - lastSeen <= 10_000 && addr.tag == remotePeerTag.toString => addr
+      yield res
+
+    override def send[V: EncodableTo[F, Format], To <: Peer: PeerTag](
+        value: V,
+        resource: Reference,
+        to: Address[To],
+    ): F[Unit] =
+      for
+        encodedValue <- encode(value)
+        payload = upickle.write((localAddress, encodedValue)).getBytes().toVector
+        _ <- session.publish(s"pslab/peers/${to.tag}/${to.clientId}", payload, AtLeastOnce)
+      yield ()
+
+    override def receive[V: DecodableFrom[F, Format], From <: Peer: PeerTag](
+        resource: Reference,
+        from: Address[From],
+    ): F[V] = getOrCreate(from).flatMap(_.take).flatMap(decode)
+
+    private def getOrCreate[P <: Peer](address: Address[P]): F[Queue[F, Array[Byte]]] = queues.get.flatMap: m =>
+      m.get(address) match
+        case Some(queue) => queue.pure[F]
+        case None =>
+          for
+            queue <- Queue.bounded[F, Array[Byte]](1)
+            _ <- queues.update(_ + (address -> queue))
+          yield queue
