0b7f20d35cd643836b5f11a95d67d28f151084f8
Luca Tassinari <luca.tassinari.2000@gmail.com>
2026-05-21 00:20:26 +0200

    complete first version of ws

--- distributed/src/it/unibo/pslab/network/ws/WebSocketNetwork.scala
+++ distributed/src/it/unibo/pslab/network/ws/WebSocketNetwork.scala
@@ -21,6 +21,9 @@ import cats.syntax.all.*
 import com.comcast.ip4s.{ host, IpAddress, Port, SocketAddress }
 import fs2.io.net.Network as Fs2Network
 import org.http4s.ember.server.EmberServerBuilder
+import sttp.capabilities.fs2.Fs2Streams
+import sttp.client4.WebSocketStreamBackend
+import sttp.client4.httpclient.fs2.HttpClientFs2Backend
 
 trait WebSocketNetwork[F[_], LP <: Peer] extends Network[F, LP, PeerRef], WS
 
@@ -28,18 +31,19 @@ object WebSocketNetwork:
 
   def make[F[_]: {Async, Concurrent, Fs2Network, NetworkMonitor}, LP <: Peer: PeerTag as localPeer](
       id: String,
-      port: Int,
+      port: Port,
       knownPeers: Map[PeerId, SocketAddress[IpAddress]],
   ): Resource[F, WebSocketNetwork[F, LP]] =
     for
       alivePeers <- Resource.eval(Ref.of[F, Map[String, Queue[F, Array[Byte]]]](Map.empty))
       incomingMsgs <- Resource.eval(Ref.of[F, IncomingMessages[F, PeerRef[?]]](IncomingMessages.empty))
       supervisor <- Supervisor[F]
-      impl = WebSocketNetworkImpl(id, port, knownPeers, alivePeers, incomingMsgs, supervisor)
+      backend = HttpClientFs2Backend.resource[F]()
+      impl = WebSocketNetworkImpl(id, port, knownPeers, supervisor, backend, alivePeers, incomingMsgs)
       _ <- EmberServerBuilder
         .default[F]
         .withHost(host"localhost")
-        .withPort(Port.fromInt(port).get) // TODO: handle invalid port
+        .withPort(port)
         .withHttpWebSocketApp(impl.setup(_).orNotFound)
         .build
     yield impl
@@ -49,14 +53,15 @@ object WebSocketNetwork:
       LP <: Peer: PeerTag as localPeer,
   ](
       id: String,
-      port: Int,
+      port: Port,
       connectedPeers: Map[PeerId, SocketAddress[IpAddress]],
+      supervisor: Supervisor[F],
+      backend: Resource[F, WebSocketStreamBackend[F, Fs2Streams[F]]],
       protected val alivePeers: Ref[F, Map[String, Queue[F, Array[Byte]]]],
       protected val incomingMsgs: Ref[F, IncomingMessages[F, PeerRef[?]]],
-      protected val supervisor: Supervisor[F],
   ) extends WebSocketNetwork[F, LP],
+        WebSocketConnector[F](supervisor, backend),
         WebSocketAcceptor[F],
-        WebSocketConnector[F],
         BaseNetwork[F, LP]:
 
     override val local: PeerRef[LP] = PeerId(localPeer, id)
@@ -68,8 +73,10 @@ object WebSocketNetwork:
         case None      => Concurrent[F].raiseError(NoSuchPeers(remotePeer))
 
     override def dispatch[To <: Peer: PeerTag](to: PeerRef[To], message: ScalaTropyMessage): F[Unit] =
-      val toAddress = connectedPeers.getOrElse(to, throw new NoSuchPeers(to.tag))
-      emit[To, ScalaTropyMessage](to, id, toAddress.toString, message)
+      for
+        toAddress <- F.fromOption(connectedPeers.get(to), new NoSuchPeers(to.tag))
+        _ <- emit(local, to, toAddress, message)
+      yield ()
 
     override def onMessage(payload: Array[Byte]): F[Unit] =
       for
--- distributed/src/it/unibo/pslab/network/ws/WsComponents.scala
+++ distributed/src/it/unibo/pslab/network/ws/WsComponents.scala
@@ -1,96 +1,136 @@
 package it.unibo.pslab.network.ws
 
-import it.unibo.pslab.network.PeerRef
-import it.unibo.pslab.peers.Peers.{ Peer, PeerTag }
+import it.unibo.pslab.network.PeerId
 
-import cats.effect.{ Async, Ref }
+import cats.effect.{ Concurrent, Ref, Resource }
 import cats.effect.std.{ Queue, Supervisor }
-import cats.implicits.{ toFlatMapOps, toFunctorOps }
 import cats.syntax.all.*
 import fs2.{ Pipe, Stream }
 import scodec.bits.ByteVector
+import sttp.capabilities.fs2.Fs2Streams
+import sttp.client4.WebSocketStreamBackend
 import upickle.default.Writer
 
-trait WebSocketHandler[F[_]]:
+trait WebSocketHandler[F[_]: Concurrent]:
+
+  /** The map of currently alive peer connections, mapping peer IDs to their outgoing message queues. */
   protected val alivePeers: Ref[F, Map[String, Queue[F, Array[Byte]]]]
 
+  /** Handles an incoming message payload from a connected peer. */
   def onMessage(payload: Array[Byte]): F[Unit]
 
-trait WebSocketAcceptor[F[_]: Async] extends WebSocketHandler[F]:
-  import org.http4s.*
+  /**
+   * Manages a WebSocket session with a connected peer, handling both incoming and outgoing messages and ensuring proper
+   * connection lifecycle management.
+   *
+   * @param peerId
+   *   the ID of the connected peer
+   * @param q
+   *   the queue for outgoing raw messages to this peer
+   * @param incomingBytes
+   *   the stream of incoming raw message payloads from this peer
+   * @return
+   *   a stream of outgoing raw message payloads to be processed and sent to the peer
+   */
+  def session(peerId: String, q: Queue[F, Array[Byte]], incomingBytes: Stream[F, Array[Byte]]): Stream[F, Array[Byte]] =
+    val incomingHandler = incomingBytes.evalMap(onMessage)
+    val outgoing = Stream.repeatEval(q.take)
+    incomingHandler.drain
+      .mergeHaltBoth(outgoing)
+      .onFinalize(alivePeers.update(_ - peerId))
+end WebSocketHandler
+
+/**
+ * Server-side WebSocket acceptor that listens and manages incoming connection requests from remote peers.
+ */
+trait WebSocketAcceptor[F[_]: Concurrent] extends WebSocketHandler[F]:
+  import org.http4s.HttpRoutes
   import org.http4s.dsl.io.*
   import org.http4s.server.websocket.WebSocketBuilder2
   import org.http4s.websocket.WebSocketFrame
 
-  def setup(wsb: WebSocketBuilder2[F]): HttpRoutes[F] = HttpRoutes.of[F]:
-    case req @ GET -> Root / "ws" / peerId =>
+  /**
+   * Sets up the HTTP routes to accept WebSocket connections and manage sessions with peers.
+   * @param builder
+   *   the WebSocketBuilder used to build WebSocket responses for incoming connection requests
+   * @return
+   *   the HTTP routes that handle WebSocket connection requests
+   */
+  def setup(builder: WebSocketBuilder2[F]): HttpRoutes[F] = HttpRoutes.of[F]:
+    case req @ GET -> Root / "ws" / connectingPeerId =>
       for
         q <- Queue.unbounded[F, Array[Byte]]
-        _ <- alivePeers.update(_ + (peerId -> q))
-        response <- wsb.build: incoming =>
-          val incomingHandler = incoming
-            .evalMap:
-              case WebSocketFrame.Binary(payload, true) => onMessage(payload.toArray)
-              case WebSocketFrame.Text(payload, true)   => onMessage(payload.getBytes)
-              case _                                    => F.unit
-          val outgoing = Stream.repeatEval(q.take).map(p => WebSocketFrame.Binary(ByteVector(p)))
-          outgoing.concurrently(incomingHandler.drain).onFinalize(alivePeers.update(_ - peerId))
+        _ <- alivePeers.update(_ + (connectingPeerId -> q))
+        response <- builder.build: incoming =>
+          session(connectingPeerId, q, incoming).map(payload => WebSocketFrame.Binary(ByteVector(payload)))
       yield response
+
+  private given Conversion[Stream[F, WebSocketFrame], Stream[F, Array[Byte]]] = _.collect:
+    case WebSocketFrame.Binary(payload, true) => payload.toArray
+    case WebSocketFrame.Text(payload, true)   => payload.getBytes
 end WebSocketAcceptor
 
-trait WebSocketConnector[F[_]: Async] extends WebSocketHandler[F]:
+/**
+ * Client-side WebSocket connector that manages outgoing connections to remote peers and emits messages to them.
+ */
+trait WebSocketConnector[F[_]: Concurrent](
+    supervisor: Supervisor[F],
+    backend: Resource[F, WebSocketStreamBackend[F, Fs2Streams[F]]],
+) extends WebSocketHandler[F]:
+  import com.comcast.ip4s.{ IpAddress, SocketAddress }
   import sttp.capabilities.fs2.Fs2Streams
   import sttp.client4.*
-  import sttp.client4.httpclient.fs2.HttpClientFs2Backend
   import sttp.client4.ws.stream.*
   import sttp.ws.WebSocketFrame
   import sttp.model.Uri
 
-  protected val supervisor: Supervisor[F]
-
-  def emit[To <: Peer: PeerTag, Value: Writer](to: PeerRef[To], selfId: String, url: String, data: Value): F[Unit] =
+  /**
+   * Emits a message towards a remote peer.
+   *
+   * @param self
+   *   the ID of the sending peer
+   * @param remote
+   *   the ID of the receiving peer
+   * @param remoteAddr
+   *   the network address of the receiving peer
+   * @param data
+   *   the message payload to send
+   */
+  def emit[Value: Writer](self: PeerId, remote: PeerId, remoteAddr: SocketAddress[IpAddress], data: Value): F[Unit] =
     for
-      _ <- alivePeers.get >>= (peers => F.catchNonFatal(println(s"**** [WebSocketNetwork] ALIVE PEERS: ${peers}")))
-      q <- alivePeers.get.flatMap(peers => peers.get(to.id).map(_.pure).getOrElse(bootstrap(to.id, selfId, url)))
+      q <- getOrBootstrap(remote, self, remoteAddr)
       payload <- F.catchNonFatal(upickle.writeBinary(data))
       _ <- q.offer(payload)
     yield ()
 
-  private def bootstrap(peerId: String, selfId: String, url: String): F[Queue[F, Array[Byte]]] =
+  private def getOrBootstrap(self: PeerId, remote: PeerId, remoteAddr: SocketAddress[IpAddress]) =
     for
       q <- Queue.unbounded[F, Array[Byte]]
-      _ <- alivePeers.update(_ + (peerId -> q))
-      _ <- supervisor.supervise(
-        openConnection(
-          url,
-          selfId,
-          incoming =>
-            val incomingHandler = incoming.evalMap:
-              case WebSocketFrame.Text(payload, true, _)   => onMessage(payload.getBytes)
-              case WebSocketFrame.Binary(payload, true, _) => onMessage(payload)
-              case _                                       => F.unit
-            val outgoing = Stream.repeatEval(q.take).map(WebSocketFrame.binary)
-            // Let incoming drive: merge them so the stream only ends when
-            // the incoming side signals close (not just when outgoing has nothing)
-            incomingHandler.drain
-              .mergeHaltBoth(outgoing) // halts when EITHER side finishes
-              .onFinalize(alivePeers.update(_ - peerId) *> Async[F].delay(println(s"CLEANUP $peerId"))),
-        ),
-      )
-    yield q
+      updated <- alivePeers.modify: peers =>
+        peers.get(remote.id) match
+          case Some(existing) => (peers, (existing, false))
+          case None           => (peers + (remote.id -> q), (q, true))
+      res <- updated match
+        case (q, true)  => supervisor.supervise(openConnection(remoteAddr, self.id, mkPipe(q, remote))).as(q)
+        case (q, false) => q.pure
+    yield res
+
+  private def mkPipe(q: Queue[F, Array[Byte]], remote: PeerId): Pipe[F, WebSocketFrame.Data[?], WebSocketFrame] =
+    incoming => session(remote.id, q, incoming).map(WebSocketFrame.binary)
 
   private def openConnection(
-      url: String,
+      remoteAddr: SocketAddress[IpAddress],
       selfId: String,
       pipe: Pipe[F, WebSocketFrame.Data[?], WebSocketFrame],
   ): F[Unit] =
-    println(s"==> [WebSocketNetwork] opening ws client to ws://${url}/ws/${selfId}")
-    HttpClientFs2Backend // TODO: parametrize
-      .resource[F]()
-      .use: backend =>
-        basicRequest
-          .get(Uri.unsafeParse("ws://" + url + "/ws/" + selfId)) // TODO: handle invalid URL
-          .response(asWebSocketStream(Fs2Streams[F])(pipe))
-          .send(backend)
-          .void
+    backend.use: backend =>
+      basicRequest
+        .get(Uri.unsafeParse("ws://" + remoteAddr.toString + "/ws/" + selfId))
+        .response(asWebSocketStream(Fs2Streams[F])(pipe))
+        .send(backend)
+        .void
+
+  private given Conversion[Stream[F, WebSocketFrame], Stream[F, Array[Byte]]] = _.collect:
+    case WebSocketFrame.Binary(payload, true, _) => payload.toArray
+    case WebSocketFrame.Text(payload, true, _)   => payload.getBytes
 end WebSocketConnector
